573743369a41ef59c10fdd0e1f38f5991b49e29a
Tejan_Gunjal <tejangunjal2@gmail.com>
2022-01-11 14:55:56 +0530

    Added to Dev branch

--- src/main/java/TestName.java
+++ src/main/java/TestName.java
@@ -1,7 +1,7 @@
 public class TestName {
 
     public static void main(String[] args){
-        System.out.println("TejaninDev");
+        System.out.println("TejaninDev123");
         System.out.println("Tejan");
     }
 }
